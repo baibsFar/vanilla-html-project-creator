bbfae00c0e45dfc3b28eeac2e6fe1fab4b8db8e4
baibs fararano <sbiab13@baibsFar>
2023-01-04 11:22:55 +0300

    feat: Adjust path of creation to current dir

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ import fs from 'fs'
 import inquirer from 'inquirer'
 import { questionsCli } from './utils/questions.js'
 import { createFile } from './utils/file-utils.js'
+import path from 'path'
 
 async function main() {
   const { indexHtml, style, projectName } = await inquirer.prompt(questionsCli, {})
   
-  const basePathProject = projectName
+  if (!process.env.BAIBS_CREA) return console.log('You must add the project path to your environment variables $PATH')
+  const basePathCreator = path.resolve(process.env.BAIBS_CREA)
+  const basePathProject = path.resolve(projectName)
   if (!fs.existsSync(basePathProject)) fs.mkdirSync(basePathProject)
 
   // Creating html file
-  createFile('./examples/index.html', `${basePathProject}/${indexHtml}`)
+  createFile(
+    path.join(basePathCreator, './examples/index.html'),
+    `${basePathProject}/${indexHtml}`
+  )
 
   // Creating style file
   const styleDir = style.toString().toLowerCase()
@@ -20,11 +26,17 @@ async function main() {
     if (!fs.existsSync(`${basePathProject}/src/${styleDir}/partials`)) fs.mkdirSync(`${basePathProject}/src/${styleDir}/partials`)
   }
   const stylePath = `${styleDir}/style.${styleDir}`
-  createFile(`./examples/${styleDir}/style.${styleDir}`, `${basePathProject}/src/${stylePath}`)
+  createFile(
+    path.join(basePathCreator, `./examples/${styleDir}/style.${styleDir}`), 
+    `${basePathProject}/src/${stylePath}`
+  )
 
   // Creating js file
   if (!fs.existsSync(`${basePathProject}/src/js`)) fs.mkdirSync(`${basePathProject}/src/js`)
-  createFile('./examples/js/main.js', `${basePathProject}/src/js/main.js`)
+  createFile(
+    path.join(basePathCreator, './examples/js/main.js'),
+    `${basePathProject}/src/js/main.js`
+  )
 }
 
 main()
